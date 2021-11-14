3b3b04a56134313ed5c0b7430e2fe56b3e4e7c64
Brian Schlining <bschlining@gmail.com>
2021-11-13 22:43:21 -0800

    Adding wrk results processing to run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+RESULTS_DIR="$SCRIPT_DIR/results"
+
 sbt "clean;stage"
 
 for framework in cask helidon http4s javalin scalatra sparkjava vertx zhttp
 do
   benchmark.sh $framework
 done
+
+WRK_APP=SCRIPT_DIR/shared/target/universal/stage/bin/shared
+$WRK_APP $RESULTS_DIR wrk1 $RESULTS_DIR/all-wrk1-results.csv
+$WRK_APP $RESULTS_DIR wrk2 $RESULTS_DIR/all-wrk2-results.csv
